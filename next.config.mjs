4c4e99d7c9f8de97c9c933db08a13e4a1ad56c73
rn0614 <rn0614@naver.com>
2025-05-16 01:12:50 +0900

    build 개발 홤경에서만 console.log 찍을 수 있도록 세팅팅

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,12 @@ import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify:true,
+  compiler:{
+    removeConsole: process.env.NODE_ENV === 'production' && {
+      exclude: ['error', 'warn']
+    }
+  },
   // src 디렉토리 사용 설정
   distDir: '.next',
   // Configure `pageExtensions`` to include MDX files
