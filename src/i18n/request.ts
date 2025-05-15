3600509dde13348c8d824f6e408475e112cf783c
rn0614 <rn0614@naver.com>
2025-05-16 01:01:23 +0900

    fix 다국어 설정 변경 하나의 json관리로 간소화화

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -2,24 +2,6 @@ import {getRequestConfig} from 'next-intl/server';
 import {hasLocale} from 'next-intl';
 import {routing} from './routing';
 
-// messages import (정적 import)
-import ko_main_menu from './messages/ko/main-menu.json';
-import en_main_menu from './messages/en/main-menu.json';
-import ko_note_edit from './messages/ko/note-edit.json';
-import en_note_edit from './messages/en/note-edit.json';
-
-// messages 객체로 통합
-const messages: Record<string, Record<string, any>> = {
-  ko: {
-    main_menu: ko_main_menu,
-    note_edit: ko_note_edit,
-  },
-  en: {
-    main_menu: en_main_menu,
-    note_edit: en_note_edit,
-  },
-};
-
 export default getRequestConfig(async (param) => {
   // Typically corresponds to the `[locale]` segment
   const requested = await param.requestLocale;
@@ -28,6 +10,6 @@ export default getRequestConfig(async (param) => {
     : routing.defaultLocale;
   return {
     locale,
-    messages: messages[locale] || messages[routing.defaultLocale],
+    messages: (await import(`@/messages/${locale}.json`)).default
   };
 });
