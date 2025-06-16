d69eeff91eaf84be5be065c2cabec34046902039
rn0614 <rn0614@naver.com>
2025-06-17 00:35:58 +0900

    fix 명칭 수정정

--- src/store/useBoxesStore.ts
+++ src/store/useBoxesStore.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 import { subscribeWithSelector } from 'zustand/middleware';
 import React from 'react';
-import { BoxData, BoxMethods } from '@/types/BoxPosition';
+import { BoxData, BoxMethods } from '@/types/boxPosition';
 
 interface BoxesState {
   boxes: Map<string, BoxData>;
